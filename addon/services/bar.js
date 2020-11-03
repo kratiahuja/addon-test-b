6a3b36f5db3a553a10742e32f5d957fc1a29e608
Krati Ahuja <kratiahuja@gmail.com>
2020-11-03 09:35:27 -0800

    iBreaking change

--- addon/services/bar.js
+++ addon/services/bar.js
@@ -1,7 +1,7 @@
 import Service from '@ember/service';
 
 export default class BarService extends Service {
-  barFunc() {
+  barFuncChanged() {
     console.log('Bar function from addon-test-b');
   }
 
