13ca9a6d71ae03dbf437a889703890d0cc77c548
Krati Ahuja <kratiahuja@gmail.com>
2020-11-02 22:38:04 -0800

    Service update

--- addon/services/bar.js
+++ addon/services/bar.js
@@ -4,4 +4,8 @@ export default class BarService extends Service {
   barFunc() {
     console.log('Bar function from addon-test-b');
   }
+
+  bazFunc() {
+    console.log('Baz function from addon-test-b');
+  }
 }
