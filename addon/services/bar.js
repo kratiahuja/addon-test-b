7fa9a60c12716e74a865115f3ec0bad175df2a57
Krati Ahuja <kratiahuja@gmail.com>
2020-11-02 22:46:14 -0800

    Undo service change

--- addon/services/bar.js
+++ addon/services/bar.js
@@ -4,8 +4,4 @@ export default class BarService extends Service {
   barFunc() {
     console.log('Bar function from addon-test-b');
   }
-
-  bazFunc() {
-    console.log('Baz function from addon-test-b');
-  }
 }
